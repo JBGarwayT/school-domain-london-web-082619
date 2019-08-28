d1a4c1b0edd66d7acc8786b20c46525da696bd39
Jean-Baptiste Garway-Templeman <jbgarwayt@gmail.com>
2019-08-28 15:44:06 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,12 +1,11 @@
 # code here!
 class School
 
+    attr_accessor :name, :roster
+
     def initialize (name)
         @name = name
         @roster = {}
     end
 
-    def roster=(roster)
-        @roster =roster
-    end
 end
